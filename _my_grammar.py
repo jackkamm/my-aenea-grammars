d7bc6fab14670fffb57f6cd2cce3dcb3480c506e
Jack Kamm <jackkamm@gmail.com>
2017-12-26 15:55:40 +0000

    intermediate

--- _my_grammar.py
+++ _my_grammar.py
@@ -44,12 +44,6 @@ class _KeystrokeRule(MappingRule):
         "tab": "tab",
         "scratch": "backspace",
 
-        # shortcuts for vim style motion
-        "drop": "c-d",
-        "sky": "c-u",
-        "scroll up": "c-y",
-        "scroll down": "c-e",
-
         # special characters
         "space": "space",
         "slap": "enter",
@@ -139,21 +133,18 @@ class _ModifierRule(MappingRule):
 
 
 class KeystrokeRule(CompoundRule):
-    spec = "[<mod1>] [<mod2>] <keystroke> [(twice|thrice|<n> ice)]"
+    spec = "[<mod1>] [<mod2>] <raw_keystroke>"
     extras = [
-        RuleRef(name="keystroke", rule=_KeystrokeRule()),
+        RuleRef(name="raw_keystroke", rule=_KeystrokeRule()),
         RuleRef(name="mod1", rule=_ModifierRule(name="m1")),
         RuleRef(name="mod2", rule=_ModifierRule(name="m2")),
-        IntegerRef("n", 1, 100)
     ]
-    defaults = {"n": 1}
 
     def value(self, node):
         root = node.children[0].children[0]
         mod1 = root.children[0].value()
         mod2 = root.children[1].value()
         stroke = root.children[2].value()
-        times = root.children[3].value()
         mod = ""
         if mod1:
             mod += mod1
@@ -162,15 +153,149 @@ class KeystrokeRule(CompoundRule):
         if mod:
             stroke = "{}-{}".format(mod, stroke)
         stroke = Key(stroke)
+        return stroke
+
+
+class MyRepeatRule(CompoundRule):
+    exported = False
+    spec = "(twice|thrice|<n> ice)"
+    extras = [IntegerRef("n", 1, 100)]
+
+    def value(self, node):
+        root = node.children[0].children[0]
+        times = root.children[0].value()
         if times == "twice":
-            times = 2
+            return 2
         elif times == "thrice":
-            times = 3
-        elif times:
-            times = times[0]
+            return 3
         else:
-            times = 1
-        return stroke * times
+            return times[0]
+
+
+my_repeat_rule = MyRepeatRule(name="my_repeat_rule")
+
+
+class RepeatableKeystrokeRule(CompoundRule):
+    spec = "<keystroke> [<times>]"
+    extras = [
+        RuleRef(name="keystroke", rule=KeystrokeRule()),
+        RuleRef(name="times", rule=my_repeat_rule)
+    ]
+
+    def value(self, node):
+        root = node.children[0].children[0]
+        stroke = root.children[0].value()
+        times = root.children[1].value()
+        if times:
+            return stroke * times
+        else:
+            return stroke
+
+
+class _KeystrokeShortcut(MappingRule):
+    mapping = {
+        # shortcuts for vim style motion
+        "drop": Key("c-d"),
+        "sky": Key("c-u"),
+        "scroll up": Key("c-y"),
+        "scroll down": Key("c-e"),
+    }
+
+
+class KeystrokeShortcut(CompoundRule):
+    spec = "<keystroke> [<times>]"
+    extras = [
+        RuleRef(name="keystroke", rule=_KeystrokeShortcut()),
+        RuleRef(name="times", rule=my_repeat_rule)
+    ]
+
+    def value(self, node):
+        root = node.children[0].children[0]
+        stroke = root.children[0].value()
+        times = root.children[1].value()
+        if times:
+            return stroke * times
+        else:
+            return stroke
+
+    def _process_recognition(self, node, extras):  # @UnusedVariable
+        node.value().execute()
+
+
+class FormatMapping(MappingRule):
+    exported = False
+    mapping = {
+        "say": aenea.format.format_natword,
+        "snake": aenea.format.format_score,
+        "studley": aenea.format.format_proper,
+        "camel": aenea.format.format_camel,
+        "jumble": aenea.format.format_jumble,
+        "dotword": aenea.format.format_dotword,
+        "dashword": aenea.format.format_dashword,
+        "sentence": aenea.format.format_sentence,
+    }
+
+
+dictation = Dictation(name="dictation")
+
+
+class MyFormatRule(CompoundRule):
+    spec = "<format_rule> <dictation>"
+    extras = [
+        RuleRef(name="format_rule", rule=FormatMapping()),
+        dictation,
+    ]
+
+    def value(self, node):
+        root = node.children[0].children[0]
+        format_rule = root.children[0].value()
+        dictation = root.children[1].value()
+        # TODO need to remove hyphens? see aenea-grammars/_multiedit.py
+        words = dictation.format().lower().split()
+        return Text(format_rule(words))
+
+
+my_format_rule = RuleRef(name="my_format_rule", rule=MyFormatRule())
+
+
+class MyLiteralRule(CompoundRule):
+    """Rule for saying MyFormatRule literally (without interruptions by other rules)"""
+    spec = "literal <my_format_rule>"
+    extras = [my_format_rule]
+
+    def _process_recognition(self, node, extras):
+        extras["my_format_rule"].execute()
+
+
+my_vocabulary_mapping = {
+    "py deaf": "def ",
+    "for loop": "for ",
+}
+
+alternatives = []
+alternatives.append(RuleRef(rule=RepeatableKeystrokeRule()))
+alternatives.append(my_format_rule)
+single_action = Alternative(alternatives)
+
+sequence = Repetition(single_action, min=1, max=16, name="sequence")
+
+
+# TODO add repeating element
+class RepeatRule(CompoundRule):
+    # Here we define this rule's spoken-form and special elements.
+    # TODO finish with format_rule?
+    spec = "<sequence>"
+    extras = [
+        sequence,  # Sequence of actions defined above.
+    ]
+
+    def _process_recognition(self, node, extras):  # @UnusedVariable
+        sequence = extras["sequence"]  # A sequence of actions.
+        for action in sequence:
+            action.execute()
+
+
+repeat_rule = RepeatRule(name="repeat_rule")
 
 
 class _EmacsKeyRule(MappingRule):
@@ -228,99 +353,47 @@ class EmacsCommandRule(CompoundRule):
         self.value(node).execute()
 
 
-class FormatMapping(MappingRule):
-    exported = False
-    mapping = {
-        "say": aenea.format.format_natword,
-        "snake": aenea.format.format_score,
-        "studley": aenea.format.format_proper,
-        "camel": aenea.format.format_camel,
-        "jumble": aenea.format.format_jumble,
-        "dotword": aenea.format.format_dotword,
-        "dashword": aenea.format.format_dashword,
-        "sentence": aenea.format.format_sentence,
-    }
-
-
-dictation = Dictation(name="dictation")
-
-
-class MyFormatRule(CompoundRule):
-    spec = "<format_rule> <dictation>"
-    extras = [
-        RuleRef(name="format_rule", rule=FormatMapping()),
-        dictation,
-    ]
-
-    def value(self, node):
-        root = node.children[0].children[0]
-        format_rule = root.children[0].value()
-        dictation = root.children[1].value()
-        # TODO need to remove hyphens? see aenea-grammars/_multiedit.py
-        words = dictation.format().lower().split()
-        return Text(format_rule(words))
-
-
-my_format_rule = RuleRef(name="my_format_rule", rule=MyFormatRule())
-
-
-class MyLiteralRule(CompoundRule):
-    """Rule for saying MyFormatRule literally (without interruptions by other rules)"""
-    spec = "literal <my_format_rule>"
-    extras = [my_format_rule]
-
-    def _process_recognition(self, node, extras):
-        extras["my_format_rule"].execute()
-
-
-my_vocabulary_mapping = {
-    "py deaf": "def ",
-    "for loop": "for ",
-}
-
-
 class MyVocabulary(MappingRule):
     exported = True
     mapping = {k: Text(v) for k, v in my_vocabulary_mapping.items()}
 
 
-alternatives = []
-alternatives.append(RuleRef(rule=KeystrokeRule()))
-alternatives.append(RuleRef(rule=MyVocabulary()))
-alternatives.append(RuleRef(rule=EmacsCommandRule()))
-alternatives.append(RuleRef(rule=EmacsKeyRule()))
-alternatives.append(my_format_rule)
-single_action = Alternative(alternatives)
-
-sequence = Repetition(single_action, min=1, max=16, name="sequence")
+prefixes = []
+prefixes.append(RuleRef(rule=MyVocabulary()))
+prefixes.append(RuleRef(rule=EmacsCommandRule()))
+prefixes.append(RuleRef(rule=EmacsKeyRule()))
+prefixes = Alternative(prefixes, name="prefixes")
 
 
-# TODO add repeating element
-class RepeatRule(CompoundRule):
-    # Here we define this rule's spoken-form and special elements.
-    # TODO finish with format_rule?
-    spec = "<sequence>"
-    extras = [
-        sequence,  # Sequence of actions defined above.
-    ]
+class MyPrefixRule(CompoundRule):
+    spec = "<prefixes> [<sequence>]"
+    extras = [prefixes, sequence]
 
     def _process_recognition(self, node, extras):  # @UnusedVariable
-        sequence = extras["sequence"]  # A sequence of actions.
-        for action in sequence:
-            action.execute()
-
-
-repeat_rule = RepeatRule(name="repeat_rule")
+        prefixes = extras["prefixes"]
+        prefixes.execute()
+        try:
+            sequence = extras["sequence"]
+        except KeyError:
+            pass
+        else:
+            for action in sequence:
+                action.execute()
 
 
 class MyMimicRule(MappingRule):
-    mapping = {"snore": Mimic("go to sleep")}
+    mapping = {
+        "snore": Mimic("go to sleep"),
+        #"natlink reload": Mimic("force natlink to reload all grammars"),
+    }
 
 
 grammar = Grammar("Generic edit")
 grammar.add_rule(repeat_rule)  # Add the top-level rule.
 grammar.add_rule(MyLiteralRule(name="literal_rule"))  # Add the top-level rule.
 grammar.add_rule(MyMimicRule())
+grammar.add_rule(KeystrokeShortcut())
+grammar.add_rule(MyPrefixRule())
 grammar.load()  # Load the grammar.
 
 
